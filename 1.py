de866dd23cf0ab06c7f72b39654ab9b01a888f99
yanshul2005y-dot <yanshul2005y@gmail.com>
2025-08-27 09:30:54 +0530

    secons class

---
+++ 1.py
@@ -0,0 +1,20 @@
+a = 4
+b = str(a)
+print(type(a))
+print(type(b))
+
+Age = 1
+
+# Camel case
+
+myVariable = "anshul"
+
+# Snake case
+
+my_variable = "anshul"
+
+# Pascal case
+
+MyVariable = "anshul"
+
+print("a")
